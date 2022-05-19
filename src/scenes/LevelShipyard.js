43bb341d09f94ac7b98f094c8e65a2a6b1b06da7
Santiago Ponce <saponce@ucsc.edu>
2022-05-18 17:52:14 -0700

    Level lights

--- src/scenes/LevelShipyard.js
+++ src/scenes/LevelShipyard.js
@@ -11,11 +11,50 @@ class LevelShipyard extends LevelBase {
     create(){
         this.createDefault();
 
+        /*Adjust player starting position */
         this.plrSpy.x = 100;
         this.plrSpy.y = 332;
+
+        this.levelLOS = new LOS(this, this.solidLayer);
+        this.createButtons();
+
+        //Sets up collision with player and lights/cameras
+        this.physics.add.overlap(this.levelLOS.ray2, this.plrSpy, function(rayFoVCircle, target){
+            if(!target.disguiseActive){
+                //console.log("detected by 2");
+                target.detectedFunc();
+            }
+        }, this.levelLOS.ray2.processOverlap.bind(this.levelLOS.ray2));
+        this.physics.add.overlap(this.levelLOS.ray, this.plrSpy, function(rayFoVCircle, target){
+            if(!target.disguiseActive){
+                //console.log("detected by 2");
+                target.detectedFunc();
+            }
+        }, this.levelLOS.ray.processOverlap.bind(this.levelLOS.ray));
     }
 
     update(time, delta){
         this.updateDefault(time, delta);
+    }
+
+    createButtons(){
+        //Create buttons
+        this.groupButtonObjs = this.add.group([new ObjInteract(this, 272, 32, 'objButton'),
+        new ObjInteract(this, 16, 208, 'objButton'), new ObjInteract(this, 528, 208, 'objButton'),
+        new ObjInteract(this, 272, 432, 'objButton')]);
+        this.groupButtonObjs.runChildUpdate = true;
+        //Create objective tracker
+        this.buttonTracker = new Checklist(this, "buttonTracker", this.groupButtonObjs.countActive());
+
+        //Add event for each button when they are pressed, listening for the signal 'objactivated'
+        let buttons = this.groupButtonObjs.getChildren(); //More like a dict than an array...
+        for(let i = 0; i < buttons.length; i++){
+        let button = buttons[i];
+        button.on('objactivated', () => {
+            this.buttonTracker.addObjective();
+        });
+        }
+        /*With ability to establish events and listeners, we could theoretically add a locked door 
+        (which I'll add later) - Santiago*/
     }
 }
