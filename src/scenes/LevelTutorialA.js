f1cef7dd72b8d59119b2f6f814698f460dee1965
Santiago Ponce <saponce@ucsc.edu>
2022-05-20 16:30:00 -0700

    Update LevelTutorialA.js

--- src/scenes/LevelTutorialA.js
+++ src/scenes/LevelTutorialA.js
@@ -16,20 +16,18 @@ class LevelTutorialA extends LevelBase {
         this.plrSpy.x = 256;
         this.plrSpy.y = 432;
 
-        //this.enemy1.enemyLOS.setAngleDegRay(this.enemy1.detection, 180);
-        //this.enemy1.straightPath(this, 432, 432, 4000);
         this.enemy1 = new Enemy(this, 608, 432, 'playerDisguise', 0, true, 200);
-        this.enemy1.enemyLOS.setAngleDegRay(this.enemy1.detection, 180);
-        this.enemy1.standingTurn(this, 4000);
+        //this.enemy1.standingTurn(this, 4000);
         //this.enemy1.straightPath(this, 432, 432, 4000);
 
         this.enemy2 = new Enemy(this, 400, 336, 'playerDisguise', 0, false, 200);
         this.enemy3 = new Enemy(this, 352, 336, 'playerDisguise', 0, true, 200);
-        this.enemy3.enemyLOS.setAngleDegRay(this.enemy3.detection, 180);
     }
 
     update(time, delta){
         this.updateDefault(time, delta);
         this.enemy1.update();
+        this.enemy2.update();
+        this.enemy3.update();
     }
 }
